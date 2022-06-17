a4ef3bde7be7770c16c39f543ee7c5f4f318af75
Ebenezr <ebenezarbukosia@gmail.com>
2022-06-17 16:57:12 +0300

    commit tree e510c24c52e4c8c2d95df4e2b47feb65321a900c

--- index.js
+++ index.js
@@ -1,5 +1,57 @@
 /* Your Code Here */
+let createEmployeeRecord = row=>{
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+let createEmployeeRecords = employeeRowData=> {
+    return employeeRowData.map(function(row){
+        return createEmployeeRecord(row)
+    })
+}
+let createTimeInEvent = dateStamp=>{
+    let [date, hour] = dateStamp.split(' ')
 
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+let createTimeOutEvent = dateStamp=>{
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+let hoursWorkedOnDate = dateSort=>{
+    let inEvent = this.timeInEvents.find(function(e){
+        return e.date === dateSort
+    })
+
+    let outEvent = this.timeOutEvents.find(function(e){
+        return e.date === dateSort
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+let wagesEarnedOnDate = dateSort=>{
+    let rawWage = hoursWorkedOnDate.call(this, dateSort)
+        * this.payPerHour
+    return parseFloat(rawWage.toString())
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -9,7 +61,7 @@
  for you to use if you need it!
  */
 
-const allWagesFor = function () {
+const allWagesFor = ()=> {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -21,3 +73,14 @@ const allWagesFor = function () {
     return payable
 }
 
+let findEmployeeByFirstName = function(srcArray, firstName) {
+    return srcArray.find(function(record){
+      return record.firstName === firstName
+    })
+  }
+
+  let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
